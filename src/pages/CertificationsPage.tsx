088e8fae607fc66787284a97d95a39e0f0abecd9
Braintech456 <info.braintechcompany@gmail.com>
2026-02-13 10:44:52 +0530

    Updated CertificationsPage.tsx

--- src/pages/CertificationsPage.tsx
+++ src/pages/CertificationsPage.tsx
@@ -1,6 +1,12 @@
 import Courses from '../components/Courses';
 import AffiliationStrip from '../components/AffiliationStrip';
 
+import PageHeader from '../components/PageHeader';
+
+<PageHeader
+  title="AFSC Certifications"
+  subtitle="Government-recognized certification programs"
+/>
 
 export default function CertificationsPage() {
   return (
