b7fdb850cafe87ea5f537dfdf56628c34c7b262a
Braintech456 <info.braintechcompany@gmail.com>
2026-02-13 10:26:19 +0530

    Updated CertificationsPage.tsx

--- src/pages/CertificationsPage.tsx
+++ src/pages/CertificationsPage.tsx
@@ -3,9 +3,11 @@ import AffiliationStrip from '../components/AffiliationStrip';
 
 export default function CertificationsPage() {
   return (
-    <>
+    <div className="min-h-screen text-black p-4">
+      <h1 className="text-2xl font-bold">CERTIFICATIONS PAGE</h1>
+
       <AffiliationStrip />
       <Courses />
-    </>
+    </div>
   );
 }
