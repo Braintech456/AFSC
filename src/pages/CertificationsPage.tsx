a5cffd311efa7d426a369f53f2e8712dd23b27ef
Braintech456 <info.braintechcompany@gmail.com>
2026-02-13 10:45:36 +0530

    Updated CertificationsPage.tsx

--- src/pages/CertificationsPage.tsx
+++ src/pages/CertificationsPage.tsx
@@ -11,7 +11,7 @@ import PageHeader from '../components/PageHeader';
 export default function CertificationsPage() {
   return (
     <div className="min-h-screen text-black p-4">
-      <h1 className="text-2xl font-bold">CERTIFICATIONS PAGE</h1>
+
 
       <AffiliationStrip />
       <Courses />
