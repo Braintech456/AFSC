0a0a0658a3f3771e2019a69d983a1fb7e312fef1
Braintech456 <info.braintechcompany@gmail.com>
2026-02-13 07:24:31 +0530

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,10 @@
 export default function HomePage() {
-  return <h1>HOME PAGE OK</h1>;
+  return (
+    <div>
+      {/* <AffiliationStrip /> */}
+      <Hero />
+      {/* <About /> */}
+      {/* <Benefits /> */}
+    </div>
+  );
 }
