c44d12c3d3b417da5878291375e72eba18b6d9bb
Braintech456 <info.braintechcompany@gmail.com>
2026-02-13 10:34:17 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-
-import HomePage from './pages/HomePage';
-import CertificationsPage from './pages/CertificationsPage';
-import VerifyPage from './pages/VerifyPage';
-import AuthorizedCentersPage from './pages/AuthorizedCentersPage';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
@@ -12,11 +6,10 @@ export default function App() {
       <Header />
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/certifications" element={<CertificationsPage />} />
-        <Route path="/verify" element={<VerifyPage />} />
-        <Route path="/authorized-centers" element={<AuthorizedCentersPage />} />
+        {/* routes */}
       </Routes>
+
+      <Footer />
     </BrowserRouter>
   );
 }
