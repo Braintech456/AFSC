7abf262fab671d9afeb8382de9eaf32391f0f522
Braintech456 <info.braintechcompany@gmail.com>
2026-02-13 07:31:09 +0530

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,30 @@
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Benefits from '../components/Benefits';
+import AffiliationStrip from '../components/AffiliationStrip';
+
 export default function HomePage() {
   return (
-    <div>
-      <Benefits />
-    </div>
+    <main className="min-h-screen bg-white">
+      {/* Affiliation strip */}
+      <section>
+        <AffiliationStrip />
+      </section>
+
+      {/* Hero section */}
+      <section>
+        <Hero />
+      </section>
+
+      {/* About section */}
+      <section>
+        <About />
+      </section>
+
+      {/* Benefits section */}
+      <section>
+        <Benefits />
+      </section>
+    </main>
   );
 }
