329b2bb0212d27a4abb049bb744d625fd059cf4b
Braintech456 <info.braintechcompany@gmail.com>
2026-02-14 21:14:07 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import CertificationsPage from './pages/CertificationsPage';
 import VerifyPage from './pages/VerifyPage';
 import AuthorizedCentersPage from './pages/AuthorizedCentersPage';
+import ContactPage from './pages/ContactPage';
 
 export default function App() {
   return (
@@ -17,8 +19,9 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/certifications" element={<CertificationsPage />} />
-        <Route path="/verify" element={<VerifyPage />} />
         <Route path="/authorized-centers" element={<AuthorizedCentersPage />} />
+        <Route path="/verify" element={<VerifyPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
 
       <Footer />
