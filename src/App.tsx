60433e126b40c93607c208e9555be47f8a0b771e
Braintech456 <info.braintechcompany@gmail.com>
2026-02-14 22:30:07 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,33 @@ import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import CertificationsPage from './pages/CertificationsPage';
+import CertificationDetailsPage from './pages/CertificationDetailsPage';
 import AuthorizedCentersPage from './pages/AuthorizedCentersPage';
 import VerifyPage from './pages/VerifyPage';
 import ContactPage from './pages/ContactPage';
 
 export default function App() {
-return (
-<BrowserRouter>
-<Header />
+  return (
+    <BrowserRouter>
+      <Header />
 
-<Routes>
-<Route path="/" element={<HomePage />} />
-<Route path="/about" element={<AboutPage />} />
-<Route path="/certifications" element={<CertificationsPage />} />
-<Route path="/authorized-centers" element={<AuthorizedCentersPage />} />
-<Route path="/verify" element={<VerifyPage />} />
-<Route path="/contact" element={<ContactPage />} />
-</Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
 
-<Footer />
-</BrowserRouter>
-);
+        {/* Certifications */}
+        <Route path="/certifications" element={<CertificationsPage />} />
+        <Route
+          path="/certifications/:id"
+          element={<CertificationDetailsPage />}
+        />
+
+        <Route path="/authorized-centers" element={<AuthorizedCentersPage />} />
+        <Route path="/verify" element={<VerifyPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
+  );
 }
